f8fb82eaa211a32886981ff8f5c3e3e034e1071c
cherrylime69 <zerolecture0922@gmail.com>
2022-07-31 17:41:19 +0900

    Feat : Bottom navigation과 Jetpack Navigation 추가
    - Navigation host 액티비티인 HomeActivity 추가
    - 소수 액티비티 + 여러 프래그먼트 화면을 위해 navigation component 및 bottom navigation 추가

--- app/src/main/java/com/example/assemble_day/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/assemble_day/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.example.assemble_day
+package com.example.assemble_day.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.assemble_day.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
