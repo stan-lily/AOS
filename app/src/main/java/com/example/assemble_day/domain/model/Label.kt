94f698dedce7882e6430dc371c6023b646b0df98
cherrylime69 <zerolecture0922@gmail.com>
2022-08-09 22:22:58 +0900

    Feat : Label 생성 화면 구현
    - Label 프래그먼트 및 ViewModel 구현
    - 타이틀, 본문, 글씨색, 배경색을 가진 Label 데이터 설계임
    - 타이틀, 본문은 EditText 로 입력받음
    - 글씨색은 스피너를 통해 하얀색, 검정색만 입력 받음
    - 배경색은 버튼 클릭시 viewModel 에서 데이터를 생성하여 랜덤하게 입력 받음
    - 사용자의 입력에 따라 저장 액션 버튼 활성화
    - 저장 버튼을 누르면 입력받은 데이터로 새 Label 생성
    - Constants 오브젝트를 통해 상수를 활용하여 가독성 높임

---
+++ app/src/main/java/com/example/assemble_day/domain/model/Label.kt
@@ -0,0 +1,8 @@
+package com.example.assemble_day.domain.model
+
+data class Label(
+    val name: String,
+    val description: String,
+    val backgroundColor: String,
+    val fontColor: String
+)
